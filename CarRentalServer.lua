8192522281bb0adcf7bd88325ac298a12e4853bf
Ömer <70106602+AryenNN@users.noreply.github.com>
2020-09-04 16:18:48 +0300

    Create CarRentalServer.lua

---
+++ CarRentalServer.lua
@@ -0,0 +1,20 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent("chargePlayer")
+AddEventHandler("chargePlayer", function(chargeAmount)
+     local xPlayer        = ESX.GetPlayerFromId(source)
+     xPlayer.removeMoney(chargeAmount)
+     CancelEvent()
+end)
+
+RegisterServerEvent("devAddPlayer")
+AddEventHandler("devAddPlayer", function(devAddAmount)
+	TriggerEvent("es:getPlayerFromId", source, function(user)
+		user.addMoney(devAddAmount)
+		CancelEvent()
+	end)
+end)
+
+--AryenN#0005
